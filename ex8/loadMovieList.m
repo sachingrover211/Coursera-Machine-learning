d596975e450f475b212bae5ed38985e52473289d
dAvE <groversachin.211@gmail.com>
2015-04-05 20:40:02 -0700

    data file removed and calling changed in the function

--- ex8/loadMovieList.m
+++ ex8/loadMovieList.m
@@ -6,7 +6,7 @@ function movieList = loadMovieList()
 
 
 %% Read the fixed movieulary list
-fid = fopen('movie_ids.txt');
+fid = fopen('../../Problems/mlclass-ex8-008/mlclass-ex8/movie_ids.txt');
 
 % Store all movies in cell array movie{}
 n = 1682;  % Total number of movies 
